43112fe7b42d57021677ae1c4b5ffd1398f8667a
Vladislav Chervyakov <chervyakovve@mail.ru>
2023-05-04 04:53:47 +0700

    feat: add footer

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 interface IProps {
   children: JSX.Element | JSX.Element[];
@@ -8,10 +9,8 @@ interface IProps {
 const Layout = ({ children }: IProps) => (
   <div className="flex min-h-screen flex-col p-0">
     <Header />
-    <main className="grow">{children}</main>
-    <footer>
-      <p>Footer</p>
-    </footer>
+    <main className="grow flex">{children}</main>
+    <Footer />
   </div>
 );
 
