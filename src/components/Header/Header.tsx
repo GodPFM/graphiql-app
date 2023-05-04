d2d188b430026e77d0903b1f1945066e7aa107d4
Daniil Asmeev <asmeev.daniil@yandex.ru>
2023-05-04 09:34:45 +0500

    fix: padding in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,11 +6,11 @@ import HeaderBurger from '@/components/ui/HeaderBurger/HeaderBurger';
 
 const Header = () => {
   return (
-    <header className="h-12 flex justify-between items-center bg-color-dark-purple">
+    <header className="h-12 flex justify-between items-center bg-color-dark-purple pl-[2%] pr-[2%]">
       <div className="h-full w-16 flex justify-center items-center border-r-color-dark-grey border-r-[1px]">
         <Image className="m-0" src={logo} alt={'logo'} />
       </div>
-      <HeaderMenu isBurger={false} classes={'mr-[44px] hidden sm:flex'} />
+      <HeaderMenu isBurger={false} classes={'hidden sm:flex'} />
       <HeaderBurger />
     </header>
   );
