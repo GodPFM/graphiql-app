e315cecd47587caa6a26d9bbe8135452431b35ff
Daniil Asmeev <asmeev.daniil@yandex.ru>
2023-05-04 09:33:10 +0500

    fix: change font weight

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,16 @@ import { Provider } from 'react-redux';
 import type { AppProps } from 'next/app';
 import Layout from '../components/Layout/Layout';
 import { setupStore } from '../store/store';
+import { DevSupport } from '@react-buddy/ide-toolbox-next';
+import { ComponentPreviews, useInitial } from '@/components/dev';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={setupStore()}>
       <Layout>
-        <Component {...pageProps} />
+        <DevSupport ComponentPreviews={ComponentPreviews} useInitialHook={useInitial}>
+          <Component {...pageProps} />
+        </DevSupport>
       </Layout>
     </Provider>
   );
