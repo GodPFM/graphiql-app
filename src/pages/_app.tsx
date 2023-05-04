96c96177854ffb74f2a778c9d2204165e1cc7f82
Daniil Asmeev <asmeev.daniil@yandex.ru>
2023-05-04 09:42:10 +0500

    fix: delete unused fi;es

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,13 @@ import '../styles/globals.css';
 import { Provider } from 'react-redux';
 import type { AppProps } from 'next/app';
 import Layout from '../components/Layout/Layout';
-import { setupStore } from '../store/store';
-import { DevSupport } from '@react-buddy/ide-toolbox-next';
-import { ComponentPreviews, useInitial } from '@/components/dev';
+import { setupStore } from '@/store/store';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={setupStore()}>
       <Layout>
-        <DevSupport ComponentPreviews={ComponentPreviews} useInitialHook={useInitial}>
-          <Component {...pageProps} />
-        </DevSupport>
+        <Component {...pageProps} />
       </Layout>
     </Provider>
   );
