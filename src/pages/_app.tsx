5146d91a67922d11c10c1584d51a3b9d15f86842
Daniil Asmeev <asmeev.daniil@yandex.ru>
2023-05-07 09:17:05 +0500

    fix: production styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,14 +5,19 @@ import type { AppProps } from 'next/app';
 
 import Layout from '../components/Layout/Layout';
 import { wrapper } from '../store/store';
+import { CssBaseline } from '@mui/material';
+import { StyledEngineProvider } from '@mui/material/styles';
 
 export function App({ Component, pageProps }: AppProps) {
   const { store, props } = wrapper.useWrappedStore(pageProps);
   return (
     <Provider store={store}>
-      <Layout>
-        <Component {...props.pageProps} />
-      </Layout>
+      <StyledEngineProvider injectFirst>
+        <CssBaseline />
+        <Layout>
+          <Component {...props.pageProps} />
+        </Layout>
+      </StyledEngineProvider>
     </Provider>
   );
 }
