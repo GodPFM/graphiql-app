14d907149b07893b4ff1e1fd43fc2d9bc8a9d5f0
Vladislav Chervyakov <chervyakovve@mail.ru>
2023-05-09 09:01:11 +0700

    feat: init introspectionQuery on server

--- src/store/reducers/document/slice.ts
+++ src/store/reducers/document/slice.ts
@@ -1,11 +1,14 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 import { RootState } from '../../store';
+import { HYDRATE } from 'next-redux-wrapper';
 
 interface InitialState {
   nav: string[];
+  schema: object;
 }
 const initialState: InitialState = {
-  nav: ['Root', 'Query', 'Example'],
+  nav: ['Root', 'Query'],
+  schema: {},
 };
 
 export const documentSlice = createSlice({
@@ -15,11 +18,29 @@ export const documentSlice = createSlice({
     addItem: (state, action: PayloadAction<string>) => {
       state.nav.push(action.payload);
     },
+    addSchema: (state, action: PayloadAction<object>) => {
+      state.schema = action.payload;
+    },
+  },
+  extraReducers: {
+    [HYDRATE]: (state, action) => {
+      console.log('HYDRATE', action.payload);
+
+      if (action.payload.document.nav) {
+        state.nav = action.payload.document.nav;
+      }
+
+      if (action.payload.document.schema) {
+        state.schema = action.payload.document.schema;
+      }
+
+      return state;
+    },
   },
 });
 
 export const selectDocument = (state: RootState) => state.document;
 
-export const { addItem } = documentSlice.actions;
+export const { addItem, addSchema } = documentSlice.actions;
 
 export default documentSlice.reducer;
