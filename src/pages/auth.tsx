9a3a9b538b36e11c592a246d29a97ae3c31cd0ab
Vladislav Chervyakov <chervyakovve@mail.ru>
2023-05-27 20:20:21 +0700

    feat: suspense

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,8 +1,12 @@
 import GraphLogoBlock from '@/components/GraphLogoBlock/GraphLogoBlock';
 import React from 'react';
-import FormAuth from '@/components/Form/FormAuth';
 import { wrapper } from '@/store/store';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import dynamic from 'next/dynamic';
+
+const FormAuth = dynamic(() => import('@/components/Form/FormAuth'), {
+  loading: () => <p>Loading...</p>,
+});
 
 const Auth = () => (
   <div className={'bg-gradient-to-br from-[#7c22ce] via-60% via-[#3c1a98] to-[#00197c] w-full'}>
